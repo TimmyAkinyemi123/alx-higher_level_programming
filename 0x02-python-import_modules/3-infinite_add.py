d021db9e0bcad1350809a3793525f64f84d6b8a2
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-08-11 14:31:39 -0700

    final checks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-        print(total)
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+    print(total)
