cc0dfbb2ea2e6862e288ecbad3c250d1f27158de
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-08-16 20:11:34 -0700

    more python tasks

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    key_list = list(a_dictionary.keys())
+    key_list.sort()
+    for i in key_list:
+        print("{}: {}".format(i, a_dictionary.get(i)))
