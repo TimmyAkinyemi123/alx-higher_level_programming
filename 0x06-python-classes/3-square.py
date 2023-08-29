dc1e726622810311adb9144a7b7b8ab08bdc9577
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-08-29 13:35:25 -0700

    pycodestyle

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Represent a square."""
-    
+
     def __init__(self, size=0):
         """Initialize a new square.
 
@@ -16,7 +16,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
