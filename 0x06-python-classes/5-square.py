8dffde73eb1b2f6e7f5031313eb8fa4f64a7a0d4
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-08-29 17:34:29 -0700

    pycodestyle

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -17,7 +17,7 @@ class Square:
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -25,7 +25,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
