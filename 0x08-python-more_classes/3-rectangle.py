d09b679c56cad5549511d4e5bc0e338a88fbd049
Timmy <akinyemitimilehin044@gmail.com>
2023-10-02 13:01:55 -0700

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-    
+
     def __str__(self):
         """Returns a printable rectangle with character #"""
         if self.__width == 0 or self.__height == 0:
@@ -58,3 +58,6 @@ class Rectangle:
         rec = []
         for i in range(self.__height):
             [rec.append'#'] for j in range(self.__width):
+                if i != self.__height - 1:
+                    rect.append("\n")
+        return ("".join(rect))
