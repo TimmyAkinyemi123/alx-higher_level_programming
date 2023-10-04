7ac585f88ae195fceda837228bdbe46ad170ac52
Timmy <akinyemitimilehin044@gmail.com>
2023-10-04 06:38:27 -0700

    task 10

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,6 +3,7 @@
 
 import sys
 
+
 def is_safe(board, row, col):
     """Checks if a queen can be placed at board[row][col]
     without conflicts.
@@ -18,22 +19,26 @@ def is_safe(board, row, col):
         if board[i][j] == 1:
             return False
 
-    for i, j in zip (range(row, len(board), 1), range(col, -1, -1)):
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
     return True
+
 
 def solve_nqueens(n):
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     board = [[0 for _ in range(n) for _ in range(n)]]
     solutions = []
 
     def solve(col):
         if col == n:
-            solutions.append([[i, row.index(1)] for i, row in enumerate(board)])
+            positions = []
+            for i, row in enumerate(board):
+                positions.append([i, row.index(1)])
+            solutions.append(positions)
             return
         for row in range(n):
             if is_safe(board, row, col):
@@ -44,14 +49,15 @@ def solve_nqueens(n):
         for solution in solutions:
             print(solution)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         N = int(sys.argv[1])
-    
+
     except ValueError:
         print("N must be a number")
         sys.exit(1)
