40d0d63e0d66dbe5470a94b57337b3eb1b2a53fc
TimmyAkinyemi123 <120695688+TimmyAkinyemi123@users.noreply.github.com>
2023-10-04 14:15:08 +0100

    task 10

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -24,4 +24,35 @@ def is_safe(board, row, col):
     return True
 
 def solve_nqueens(n):
-    if
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    board = [[0 for _ in range(n) for _ in range(n)]]
+    solutions = []
+
+    def solve(col):
+        if col == n:
+            solutions.append([[i, row.index(1)] for i, row in enumerate(board)])
+            return
+        for row in range(n):
+            if is_safe(board, row, col):
+                board[row][col] = 1
+                solve(col + 1)
+                board[row][col] = 0
+        solve(0)
+        for solution in solutions:
+            print(solution)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        N = int(sys.argv[1])
+    
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    solve_nqueens(N)
