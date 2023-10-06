d950383794773ac7975cd89b184555689240b5e9
Timmy <akinyemitimilehin044@gmail.com>
2023-10-06 12:32:14 -0700

    0x01

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 2-matrix_divided.py
+
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix.
+    Args:
+            matrix(list): A list of lists of ints and floats
+            div(int/float): The divisor
+    Raises:
+        TypeError: If the matrix contains a non-number
+        TyperError: If the matrix contains rows of different sizes
+        TypeError: If div is not an int or float
+        ZeroDivisionError: If div is 0
+    Returns:
+        A new matrix
+    """"
+
+
