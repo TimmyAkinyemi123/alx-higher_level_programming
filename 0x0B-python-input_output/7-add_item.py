829170981ea131b83901ea1b86a80eaba621912f
TimmyAkinyemi123 <120695688+TimmyAkinyemi123@users.noreply.github.com>
2023-10-10 20:28:24 +0100

    t7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,9 +5,10 @@ import sys
 from os import path
 from importlib import import_module
 
-if not path.isfile("5-save_to_json_file.py") or not 
-path.isfile("6-load_from_json_file.py"):
-    raise ImportError("5-save_to_json_file.py and 6-load_from_json_file.py are required.")
+if not path.isfile("5-save_to_json_file.py") or not path.isfile("6-load_from_json_file.py"):
+    raise ImportError(
+        "5-save_to_json_file.py and 6-load_from_json_file.py are required."
+    )
 
 save_to_json_file = import_module("5-save_to_json_file").save_to_json_file
 load_from_json_file = import_module("6-load_from_json_file").load_from_json_file
@@ -16,5 +17,6 @@ try:
     items = load_from_json_file("add_item.json")
 except FileNotFoundError:
     items = []
+
 items.extend(sys.argv[1:])
 save_to_json_file(items, "add_item.json")
