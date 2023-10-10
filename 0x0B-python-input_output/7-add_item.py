8cfa9e01adf1b261a10d631b4e0ad1e5f848c3ea
TimmyAkinyemi123 <120695688+TimmyAkinyemi123@users.noreply.github.com>
2023-10-10 20:22:31 +0100

    fixed task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,19 @@
 
 """Add all arguments to a Python list and save them to a file."""
 import sys
+from os import path
+from importlib import import_module
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+if not path.isfile("5-save_to_json_file.py") or not path.isfile(
+    "6-load_from_json_file.py"):
+    raise ImportError("5-save_to_json_file.py and 6-load_from_json_file.py are required.")
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file = import_module("5-save_to_json_file").save_to_json_file
+load_from_json_file = import_module("6-load_from_json_file").load_from_json_file
+
+try:
+    items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, "add_item.json")
