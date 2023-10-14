227d09270bc9c7e7e6e41e634bdd821884e70472
Timmy <akinyemitimilehin044@gmail.com>
2023-10-14 15:43:34 -0700

    task 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 """Defines the class Base"""
+import json
+import csv
+import os.path
 
 
 class Base:
     """Represents a Base"""
     __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
