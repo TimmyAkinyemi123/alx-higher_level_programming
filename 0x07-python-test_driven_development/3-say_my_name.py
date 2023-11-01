71656e4166c93b683596eb7d21500df573019e6b
TimmyAkinyemi123 <120695688+TimmyAkinyemi123@users.noreply.github.com>
2023-11-01 22:28:49 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,7 +6,7 @@ def say_my_name(first_name, last_name=""):
     """ prints first and last name
         Arguments:
             @first_name: first name to be printed
-            @second_name: last_name to be printed
+            @last_name: last_name to be printed
     """
 
     if type(first_name) is not str:
