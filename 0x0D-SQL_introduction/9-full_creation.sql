74870560e37558f1c8176cc4959178d4cefe211f
TimmyAkinyemi123 <120695688+TimmyAkinyemi123@users.noreply.github.com>
2023-11-15 00:15:13 +0100

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+-- This script creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (
 	  id INT,
 	  name VARCHAR(256),
