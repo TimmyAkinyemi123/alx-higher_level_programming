5c729d66acbd64ce48cd3fad5e6e3ecf80705fdd
Timmy <akinyemitimilehin044@gmail.com>
2023-12-20 18:50:43 -0800

    added readme

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa:
+    take 3 arguments: 
+        mysql username, mysql password and database name"""
 import MySQLdb
 from sys import argv
 
