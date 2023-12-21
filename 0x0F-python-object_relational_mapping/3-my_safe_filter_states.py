5f9557292bad9bd8eded9e79e218992e88abecb6
Timmy <akinyemitimilehin044@gmail.com>
2023-12-20 19:07:51 -0800

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-displays all values in the states table of 
-hbtn_0e_0_usa where name matches the argument"""
+displays all values in the states table of
+btn_0e_0_usa where name matches the argument"""
 
 from sys import argv
 import MySQLdb
