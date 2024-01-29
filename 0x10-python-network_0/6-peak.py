f40aeae5c92919a804f3020afa46e2f0930c9422
Timmy <akinyemitimilehin044@gmail.com>
2024-01-29 12:41:37 -0800

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Finds the peak in a list of ints"""
+
+
+def find_peak(list_of_integers):
+    """ Finds the peak in a list of ints"""
+    if not list_of_integers:
+        return None
+    low, high = 0, len(list_of_integers) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+    return list_of_integers[low]
