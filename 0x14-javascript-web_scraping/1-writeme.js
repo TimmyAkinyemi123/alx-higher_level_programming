f47da12875cf4af4ca8324ac7f153a1cf43be69c
KishitheCreator <120695688+TimmyAkinyemi123@users.noreply.github.com>
2024-02-21 21:45:55 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-function writeToFile (filePath, content) {
-  try {
-    fs.writeFileSync(filePath, content, 'utf-8');
-  } catch (error) {
-    console.error(error);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) {
+    console.log(error);
   }
-}
-
-if (process.argv.length !== 4) {
-  console.error('Usage: ./1-writeme.js<file_path> <content>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-writeToFile(filePath, content);
+});
