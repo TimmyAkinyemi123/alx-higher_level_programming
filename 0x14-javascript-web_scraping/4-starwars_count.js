0efcb6aa1063e169eab55594d412b22ac2a7356e
KishitheCreator <120695688+TimmyAkinyemi123@users.noreply.github.com>
2024-02-21 21:47:31 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,28 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: ./starwars_count.js <api_url>');
-  process.exit(1);
-}
-
-const apiUrl = process.argv[2];
-const characterId = 18;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error.message);
-  } else if (response.statusCode === 200) {
-    const filmsData = JSON.parse(body);
-    const count = filmsData.results.reduce((acc, film) => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        return acc + 1;
-      }
-      return acc;
-    }, 0);
-
-    console.log(count);
-  } else {
-    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
