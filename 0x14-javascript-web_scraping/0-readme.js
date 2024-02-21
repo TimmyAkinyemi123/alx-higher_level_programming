bd5d06948ebbcb2779c7a369f8222560510b4684
KishitheCreator <120695688+TimmyAkinyemi123@users.noreply.github.com>
2024-02-21 21:39:26 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,19 +1,11 @@
-#!/usr/bin/node
 const fs = require('fs');
 
-function readAndPrintFile (filePath) {
-  try {
-    const content = fs.readFileSync(filePath, 'utf-8');
-    console.log(content);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-readme.js<file_path>');
-  process.exit(1);
-}
-
 const filePath = process.argv[2];
-readAndPrintFile(filePath);
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
